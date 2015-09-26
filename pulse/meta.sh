881bf701fb331f0ce152f107f173b05225281923
ed <tripflag@gmail.com>
2015-09-27 01:53:03 +0200

    fkin weebs

--- pulse/meta.sh
+++ pulse/meta.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 #
 # part of shit-pulse
 # Shellscript Icecast Tools for PulseAudio
@@ -8,6 +8,25 @@
 # edit the path below
 DEADBEEF='/home/ed/bin/deadbeef/deadbeef'
 
-test -z "${1}" && echo "no data"
-test x"${1}" = "xartist" && "$DEADBEEF" --nowplaying %a 2>/dev/null
-test x"${1}" = "xtitle"  && "$DEADBEEF" --nowplaying %t 2>/dev/null
+set -o pipefail
+function get()
+{
+	type='a'
+	[ "x$1" == "xt" ] && type=t
+	
+	# read tag from deadbeef
+	orig="$("$DEADBEEF" --nowplaying %$type 2>/dev/null)"
+	
+	# try to decode weeaboo shit
+	sjis="$(IFS= echo "$orig" | iconv -t latin1 2>/dev/null | iconv -f sjis 2>/dev/null)"
+	
+	# decode ok? if yes, keep it
+	[ $? -eq 0 ] &&
+		IFS= echo -n "$sjis" ||
+		IFS= echo -n "$orig"
+}
+
+# broilerplate
+[ -z "$1" ] && echo "no data"
+[ "x$1" = "xartist" ] && get a
+[ "x$1" = "xtitle" ] && get t
